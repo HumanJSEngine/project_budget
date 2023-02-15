8746b7fe762031f0b61c86742da4c67c369a0326
bmj <bmj44571627@gmail.com>
2023-02-15 12:58:47 +0900

    리스트 작업 중

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import BottomNavigation from '../components/common/BottomNavigation';
 import Page from '../styles/Page';
+import Expenditure from '../components/calendar/Expenditure';
+import Datelist from '../components/calendar/Datelist';
+import Perdaytotal from '../components/calendar/Perdaytotal';
+import ExpendList from '../components/calendar/ExpendList';
+import TitleList from '../components/calendar/TitleList';
+import Title from '../components/calendar/Title';
+import Category from '../components/calendar/Category';
+import Price from '../components/calendar/Price';
 
 const List = () => {
-  return (
-    <Page>
-      List
-      <BottomNavigation />
-    </Page>
-  );
+    return (
+        <Page>
+            <Expenditure>
+                <Datelist date={'1일'} weekday={'월요일'} />
+                <Perdaytotal counts={6} amounts={10000} />
+                <hr />
+                <ExpendList>
+                    <TitleList>
+                        <Title title={'제목입니다'} />
+                        <Category
+                            culture={'문화분류'}
+                            place={'장소'}
+                            payment={'결제수단'}
+                        ></Category>
+                    </TitleList>
+                    <Price price={10000} />
+                </ExpendList>
+                <ExpendList>
+                    <TitleList>
+                        <Title title={'제목입니다'} />
+                        <Category
+                            culture={'문화분류'}
+                            place={'장소'}
+                            payment={'결제수단'}
+                        ></Category>
+                    </TitleList>
+                    <Price price={10000} />
+                </ExpendList>
+            </Expenditure>
+            <BottomNavigation />
+        </Page>
+    );
 };
 
 export default List;
