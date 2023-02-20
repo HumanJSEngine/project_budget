083b473aa184d800eb1dde7f97f4451867b215d2
bmj <bmj44571627@gmail.com>
2023-02-20 17:58:33 +0900

    스크롤 구현 중

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import styled from 'styled-components';
 import BottomNavigation from '../components/common/BottomNavigation';
 import ShowCalendar from '../components/ui/ShowCalendar';
@@ -16,27 +16,85 @@ import 'react-calendar/dist/Calendar.css';
 import 'moment/locale/ko';
 import useFetch from '../hooks/useFetch';
 import GetTotal from '../utils/GetTotal';
+import axios from 'axios';
 
 const Calendar = () => {
-    const caldata = useFetch(
-        'get',
-        'http://haeji.mawani.kro.kr:8585/api/expense/list'
-    );
+    // const caldata = useFetch(
+    //     'get',
+    //     'http://haeji.mawani.kro.kr:8585/api/expense/list'
+    // );
+
+    const [result, setResult] = useState([]);
+    const [item, setItem] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getFetchData = async () => {
+        await axios
+            .get('http://haeji.mawani.kro.kr:8585/api/expense/list')
+            .then((res) => {
+                let response = res.data.expense;
+                setResult(response.slice(0, 5));
+                response = response.slice(5);
+                setItem(response);
+                setIsLoading(false);
+            });
+    };
+
+    const _infiniteScroll = useCallback(() => {
+        let scrollHeight = Math.max(
+            document.documentElement.scrollHeight,
+            document.body.scrollHeight
+        );
+        let scrollTop = Math.max(
+            document.documentElement.scrollTop,
+            document.body.scrollTop
+        );
+        let clientHeight = document.documentElement.clientHeight;
+        scrollHeight -= 1000;
+        if (scrollTop + clientHeight >= scrollHeight && isLoading === false) {
+            fetchMoreData();
+        }
+    }, [isLoading]);
+
+    useEffect(() => getFetchData(), []);
+
+    useEffect(() => {
+        window.addEventListener('scroll', _infiniteScroll, true);
+        return () => {
+            window.removeEventListener('scroll', _infiniteScroll, true);
+        };
+    }, [_infiniteScroll]);
+
+    const fetchMoreData = async () => {
+        setIsLoading(true);
+        setResult(result.concat(item.slice(0, 10)));
+        setItem(item.slice(10));
+
+        if (item.length > 15) {
+            const timer = setTimeout(() => {
+                setResult(result.concat(item.slice(0, 20)));
+                setItem(item.slice(20));
+                if (item.length === 25) clearTimeout(timer);
+            }, 1000);
+        }
+
+        setIsLoading(false);
+    };
 
     return (
         <Page>
             <Header title={'제목'} />
             <CalendarWrap>
-                <ShowCalendar caldata={caldata} />
+                <ShowCalendar caldata={result} />
             </CalendarWrap>
             <Expenditure>
                 <Datelist date={'1일'} weekday={'월요일'} />
                 <Perdaytotal
-                    counts={caldata.length}
-                    amounts={GetTotal(caldata)}
+                    counts={result.length}
+                    amounts={GetTotal(result)}
                 />
                 <hr />
-                {caldata.map(
+                {result.map(
                     ({ ehSeq, ehTitle, ehCcSeq, ehLocation, ehPrice }) => (
                         <ExpendList key={ehSeq}>
                             <TitleList>
@@ -51,6 +109,7 @@ const Calendar = () => {
                         </ExpendList>
                     )
                 )}
+                {setIsLoading && <span>로딩중</span>}
             </Expenditure>
             <BottomNavigation />
         </Page>
